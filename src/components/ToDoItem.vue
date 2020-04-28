67aec6e71cd2cf1a6c03f2fbc7a6a223be6f7810
vakyla98 <vakyla98@ukr.net>
2020-04-28 17:38:01 +0300

    Added Modal window to change Todos

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,9 +1,8 @@
 <template>
   <li class="todolist__item" :id="todo.id">
-      <p>{{todo.text}}</p>
+      <span>{{todo.text}}</span>
       <button @click="$emit('delete',todo.id)">Delete</button>
-    <!-- <button type="button" class="btn" @click="$emit('edit', user.id)">Edit name</button>
-    <button type="button" class="btn" @click="$emit('delete', user.id)">Delete user</button> -->
+      <button @click="$emit('edit',todo.id)">Edit</button>
   </li>
 </template>
 
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -3,9 +3,16 @@
     <h1 class="app-name">My second app</h1>
     <CreateToDo @add="addHandler" />
     <ul class="todo__list" v-if="todos.length">
-      <ToDoItem v-for="todo in todos" :key="todo.id" :todo="todo" @delete="deleteHandler" />
+      <ToDoItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @delete="deleteHandler"
+        @edit="editHandler"
+      />
     </ul>
     <p v-else>No items</p>
+    <Modal v-if="isModalVisible" :text="this.todos[targetIndex].text" @close="changeTextHandler"/>
   </div>
 </template>
 
@@ -13,22 +20,37 @@
 import { v4 } from "uuid";
 import ToDoItem from "./ToDoItem.vue";
 import CreateToDo from "./CreateToDo.vue";
+import Modal from "./Modal.vue";
 export default {
   data() {
     return {
-      todos: []
+      todos: [],
+      targetIndex: Number,
+      isModalVisible: false
     };
   },
   components: {
     CreateToDo,
-    ToDoItem
+    ToDoItem,
+    Modal
   },
   methods: {
     addHandler(text) {
       this.todos.push({ id: v4(), text });
     },
-    deleteHandler(id){
-        this.todos = this.todos.filter( todo => todo.id !== id)
+    editHandler(id) {
+      this.targetIndex = this.todos.findIndex(todo => todo.id === id);
+      this.isModalVisible = true;
+    },
+    changeTextHandler(newText) {
+      this.todos[this.targetIndex].text = newText;
+      console.log("target inedx: " + this.targetIndex);
+      console.log("new text: " + newText);
+      console.log("apply that: " + this.todos[this.targetIndex].text);
+      this.isModalVisible = false;
+    },
+    deleteHandler(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
     }
   }
 };
